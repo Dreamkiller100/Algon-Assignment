5f80e0e96ed334119d80699c7992c140d2f49c47
NAGUL JAGADISH <naguljagad10@gmail.com>
2025-06-11 17:14:01 +0530

    Update anagram.js

--- Day-1 Assignment/Easy/anagram.js
+++ Day-1 Assignment/Easy/anagram.js
@@ -1,8 +1,3 @@
-/*
-  Write a function `isAnagram` which takes 2 parameters and returns true/false if those are anagrams or not.
-  What's Anagram?
-  - A word, phrase, or name formed by rearranging the letters of another, such as spar, formed from rasp.
-*/
 
 /*
   Write a function `isAnagram` which takes 2 parameters and returns true/false if those are anagrams or not.
