dfded924b2a2724962caae7e78a916147419fe0a
NAGUL JAGADISH <naguljagad10@gmail.com>
2025-06-18 02:06:45 +0530

    Update server.js

--- Day-2 Assignment/Fileserverproject/server.js
+++ Day-2 Assignment/Fileserverproject/server.js
@@ -4,3 +4,4 @@ const app = require('./fileserver');
 app.listen(3000, () => {
   console.log("File server running at http://localhost:3000/files");
 });
+/* just go to http://localhost:3000/files */
