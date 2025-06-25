ec9df80e324d84371a6e49e6a1fffc2d7e7c949a
NAGUL JAGADISH <naguljagad10@gmail.com>
2025-06-26 03:24:51 +0530

    Update index.js

--- Day-4 Assignment/index.js
+++ Day-4 Assignment/index.js
@@ -1,1 +1,6 @@
+const { Blockchain, Block } = require('./blockchain');
 
+const myCoin = new Blockchain();
+console.log("Mining block...");
+myCoin.addBlock(new Block(1, Date.now().toString(), { amount: 100 }));
+console.log("Is blockchain valid?", myCoin.isChainValid());
